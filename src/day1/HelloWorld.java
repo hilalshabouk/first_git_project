1bc2c280146f87f267c65cb055455b1ee39ae2b4
hilalshabouk <170339687+hilalshabouk@users.noreply.github.com>
2024-12-12 10:32:54 +0000

    Update HelloWorld.java

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -3,5 +3,14 @@ package day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        System.out.println("Hello Cydeo " );
+        
+        System.out.println("Hello Programmers")
+      
+       
+        
+       
+        
     }
 }
